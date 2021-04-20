0757a13177b7677b2fcd8609fe58531c3ff3a1bf
okesseko <59650459+okesseko@users.noreply.github.com>
2021-04-20 17:30:54 +0800

    使用content管理theme

--- dark-mode/src/common/containers/App.js
+++ dark-mode/src/common/containers/App.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { createContext, useState } from "react";
 
+export const ThemeMode = createContext({
+  mode: "light",
+  setMode: () => {},
+});
 export default function App({ children }) {
-  return children;
+  const [mode, setMode] = useState("light");
+  const values = { mode, setMode };
+  return <ThemeMode.Provider value={values}>{children}</ThemeMode.Provider>;
 }
